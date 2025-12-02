77f1aabc922b340321a6d04aec934334ee1bf64d
Başar <35398147+b68h4@users.noreply.github.com>
2025-12-03 01:34:02 +0300

    fix: push custom image loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+    reactStrictMode: true,
+    images: {
+        loader: "custom",
+        loaderFile: "./imageLoader.ts",
+    },
 };
 
 export default nextConfig;
